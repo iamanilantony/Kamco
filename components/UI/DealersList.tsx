dfabdd2215c9d5af06d297f3950b1592234e2e45
iamanilantony <anilantony3134@gmail.com>
2024-11-05 13:51:06 +0530

    chore: updated dealers list

--- components/UI/DealersList.tsx
+++ components/UI/DealersList.tsx
@@ -1,66 +1,9 @@
 // pages/dealers.js
 
 import Link from 'next/link';
+import dealers from '@/public/data/dealers'
 
 export default function DealersPage() {
-    const dealers = [
-        {
-            id: 1,
-            name: 'Kottaram Traders',
-            address: 'Integra VIP, 1st Floor, Opp. Rahman Hospital, V.I.P Road, Six Mile, Guwahati-781 022, Assam State',
-            phone: '09401981187, 08638027350',
-        },
-        {
-            id: 2,
-            name: 'Kottaram Traders',
-            address: 'Integra VIP, 1st Floor, Opp. Rahman Hospital, V.I.P Road, Six Mile, Guwahati-781 022, Assam State',
-            phone: '09401981187, 08638027350',
-        },
-        {
-            id: 3,
-            name: 'Kottaram Traders',
-            address: 'Integra VIP, 1st Floor, Opp. Rahman Hospital, V.I.P Road, Six Mile, Guwahati-781 022, Assam State',
-            phone: '09401981187, 08638027350',
-        },
-        {
-            id: 4,
-            name: 'Kottaram Traders',
-            address: 'Integra VIP, 1st Floor, Opp. Rahman Hospital, V.I.P Road, Six Mile, Guwahati-781 022, Assam State',
-            phone: '09401981187, 08638027350',
-        },
-        {
-            id: 5,
-            name: 'Kottaram Traders',
-            address: 'Integra VIP, 1st Floor, Opp. Rahman Hospital, V.I.P Road, Six Mile, Guwahati-781 022, Assam State',
-            phone: '09401981187, 08638027350',
-        },
-        {
-            id: 6,
-            name: 'Kottaram Traders',
-            address: 'Integra VIP, 1st Floor, Opp. Rahman Hospital, V.I.P Road, Six Mile, Guwahati-781 022, Assam State',
-            phone: '09401981187, 08638027350',
-        },
-        {
-            id: 7,
-            name: 'Kottaram Traders',
-            address: 'Integra VIP, 1st Floor, Opp. Rahman Hospital, V.I.P Road, Six Mile, Guwahati-781 022, Assam State',
-            phone: '09401981187, 08638027350',
-        },
-        {
-            id: 8,
-            name: 'Kottaram Traders',
-            address: 'Integra VIP, 1st Floor, Opp. Rahman Hospital, V.I.P Road, Six Mile, Guwahati-781 022, Assam State',
-            phone: '09401981187, 08638027350',
-        },
-        {
-            id: 9,
-            name: 'Kottaram Traders',
-            address: 'Integra VIP, 1st Floor, Opp. Rahman Hospital, V.I.P Road, Six Mile, Guwahati-781 022, Assam State',
-            phone: '09401981187, 08638027350',
-        },
-        // Add more dealers as needed
-    ];
-
     return (
         <div className="px-4 md:px-10 lg:px-20 py-10 font-sans">
             <h1 className="text-3xl font-bold mb-6">Authorized Dealers</h1>
@@ -93,8 +36,8 @@ export default function DealersPage() {
                     <Link href={`/dealers/${dealer.id}`} key={index}>
                         <div className="p-6 border rounded-lg shadow-sm bg-white text-left hover:bg-[#166434] hover:text-white cursor-pointer transition duration-300 ease-in-out">
                             <div className="flex items-center mb-4">
-                                <div className={`w-10 h-10 rounded-full bg-gray-300`} />
-                                <h2 className={`ml-4 text-lg font-bold`}>{dealer.name}</h2>
+                                {/* <div className={`w-10 h-10 rounded-full bg-gray-300`} /> */}
+                                <h2 className={`ml-0 text-lg font-bold`}>{dealer.name}</h2>
                             </div>
                             <p className={`text-sm`}>{dealer.address}</p>
                             <p className={`text-sm mt-2`}>Tel: {dealer.phone}</p>
