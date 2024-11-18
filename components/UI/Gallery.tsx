6510ac2469e862a39a5e849ddcda73116fd4bb3e
iamanilantony <anilantony3134@gmail.com>
2024-11-18 09:48:07 +0530

    chore: gallery image not working

--- components/UI/Gallery.tsx
+++ components/UI/Gallery.tsx
@@ -5,19 +5,19 @@ import Link from 'next/link';
 
 const GallerySection = () => {
     const galleryItems = [
-        { image: "gallery/13.jpg" },
-        { image: "gallery/2.jpg" },
-        { image: "gallery/3.jpg" },
-        { image: "gallery/4.jpg" },
-        { image: "gallery/5.jpg" },
-        { image: "gallery/6.jpg" },
-        { image: "gallery/7.jpg" },
-        { image: "gallery/8.jpg" },
-        { image: "gallery/9.jpg" },
-        { image: "gallery/10.jpg" },
-        { image: "gallery/11.jpg" },
-        { image: "gallery/12.jpg" },
-        { image: "gallery/13.jpg" }, 
+        { image: "/gallery/13.jpg" },
+        { image: "/gallery/2.jpg" },
+        { image: "/gallery/3.jpg" },
+        { image: "/gallery/4.jpg" },
+        { image: "/gallery/5.jpg" },
+        { image: "/gallery/6.jpg" },
+        { image: "/gallery/7.jpg" },
+        { image: "/gallery/8.jpg" },
+        { image: "/gallery/9.jpg" },
+        { image: "/gallery/10.jpg" },
+        { image: "/gallery/11.jpg" },
+        { image: "/gallery/12.jpg" },
+        { image: "/gallery/13.jpg" }, 
     ];
 
     const gallerySettings = {
