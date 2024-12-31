efddd11f1098799bafc17336d1a08d7186062ecf
Vinu Devaraj <vinu.devaraj@logidots.com>
2024-12-31 13:05:18 +0530

    feat: responsive issues fix

--- app/right-to-information/page.tsx
+++ app/right-to-information/page.tsx
@@ -3,65 +3,63 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="flex flex-col md:flex-row justify-center py-10 md:mx-20">
-      {/* Left Side - Contact Information */}
-      <div className="md:w-max p-6 py-12 border-2 border-[#166434] md:rounded-2xl rounded-xl flex flex-col justify-start mx-auto md:mx-16 mb-8 md:mb-0">
-        {/* Centered on mobile */}
-        <h2 className="text-2xl font-semibold mb-6 text-left">
-          Important Links
-        </h2>
-        <ol className="list-decimal pl-5 space-y-4 text-left text-base font-medium text-gray-700">
-          <li>
-            <Link
-              href="/Docs/PIO.pdf"
-              target="_blank"
-              className="cursor-pointer hover:bg-gray-100 rounded-md block"
-            >
-              List of Public Information Officers
-            </Link>
-          </li>
-          <li>
-            Query Disposition Status:
-            <span className="block mt-2">
-              <Link
-                href="https://www.kamcoindia.com/ckeditor_files/1729853675.pdf"
-                target="_blank"
-                className="cursor-pointer hover:bg-gray-100 rounded-md p-2 block"
-              >
-                2023
-              </Link>
-            </span>
-            <span className="block mt-2">
-              <Link
-                href="https://www.kamcoindia.com/ckeditor_files/1729853656.pdf"
-                target="_blank"
-                className="cursor-pointer hover:bg-gray-100 rounded-md p-2 block"
-              >
-                2022
-              </Link>
-            </span>
-            <span className="block mt-2">
-              <Link
-                href="https://www.kamcoindia.com/ckeditor_files/1729853636.pdf"
-                target="_blank"
-                className="cursor-pointer hover:bg-gray-100 rounded-md p-2 block"
-              >
-                2021
-              </Link>
-            </span>
-            <span className="block mt-2">
-              <Link
-                href="https://www.kamcoindia.com/userfiles/RIA%20-Register_Updated_ason_DEC2021(2).pdf"
-                target="_blank"
-                className="cursor-pointer hover:bg-gray-100 rounded-md p-2 block"
-              >
-                BEFORE 2020
-              </Link>
-            </span>
-            (Click on the required year)
-          </li>
-        </ol>
-      </div>
-    </div>
+     <div className='flex flex-col md:flex-row justify-center py-10 md:mx-20 mt-16'>
+        {/* Left Side - Contact Information */}
+        <div className='md:w-max p-6 py-12 border-2 border-[#166434] md:rounded-2xl rounded-xl flex flex-col justify-start mx-auto md:mx-16 mb-8 md:mb-0'>
+           {/* Centered on mobile */}
+           <h2 className='text-2xl font-semibold mb-6 text-left'>Important Links</h2>
+           <ol className='list-decimal pl-5 space-y-4 text-left text-base font-medium text-gray-700'>
+              <li>
+                 <Link
+                    href='/Docs/PIO.pdf'
+                    target='_blank'
+                    className='cursor-pointer hover:bg-gray-100 rounded-md block'
+                 >
+                    List of Public Information Officers
+                 </Link>
+              </li>
+              <li>
+                 Query Disposition Status:
+                 <span className='block mt-2'>
+                    <Link
+                       href='https://www.kamcoindia.com/ckeditor_files/1729853675.pdf'
+                       target='_blank'
+                       className='cursor-pointer hover:bg-gray-100 rounded-md p-2 block'
+                    >
+                       2023
+                    </Link>
+                 </span>
+                 <span className='block mt-2'>
+                    <Link
+                       href='https://www.kamcoindia.com/ckeditor_files/1729853656.pdf'
+                       target='_blank'
+                       className='cursor-pointer hover:bg-gray-100 rounded-md p-2 block'
+                    >
+                       2022
+                    </Link>
+                 </span>
+                 <span className='block mt-2'>
+                    <Link
+                       href='https://www.kamcoindia.com/ckeditor_files/1729853636.pdf'
+                       target='_blank'
+                       className='cursor-pointer hover:bg-gray-100 rounded-md p-2 block'
+                    >
+                       2021
+                    </Link>
+                 </span>
+                 <span className='block mt-2'>
+                    <Link
+                       href='https://www.kamcoindia.com/userfiles/RIA%20-Register_Updated_ason_DEC2021(2).pdf'
+                       target='_blank'
+                       className='cursor-pointer hover:bg-gray-100 rounded-md p-2 block'
+                    >
+                       BEFORE 2020
+                    </Link>
+                 </span>
+                 (Click on the required year)
+              </li>
+           </ol>
+        </div>
+     </div>
   );
 }
