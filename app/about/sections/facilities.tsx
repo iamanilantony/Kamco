f62838b2923a393807b108c5bd90eb373f464945
iamanilantony <anilantony3134@gmail.com>
2025-01-03 14:24:52 +0530

    chore: gallery pagination

--- app/about/sections/facilities.tsx
+++ app/about/sections/facilities.tsx
@@ -15,7 +15,7 @@ const Facilities = () => {
       imageUrl: "/Units/ATHANI.JPG",
     },
     {
-      title: "Kalamassery Unit - Diesel Engine E...",
+      title: "Kalamassery Unit – Diesel Engine Expertise.",
       description:
         "Our second unit, established in 1992 in Kalamassery, specializes in Diesel Engine production, forming the core of our Power Tiller and Tractor lineup.",
       imageUrl: "/Units/KALAMASSERY.jpeg",
